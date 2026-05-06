3bb607fe720d5e02bde675b7d324fbc7cfce9641
Lamyaa <109660921+Lamyaa439@users.noreply.github.com>
2026-05-06 23:44:02 +0300

    Update main.ts

--- task_1/js/main.ts
+++ task_1/js/main.ts
@@ -3,7 +3,6 @@ interface Teacher {
   // readonly ensures these can only be set during initialization
   readonly firstName: string;
   readonly lastName: string;
-  
   fullTimeEmployee: boolean;
   location: string;
   
@@ -12,6 +11,11 @@ interface Teacher {
 
   // Index signature to allow adding any other attributes
   [propName: string]: any;
+}
+
+// New Directors interface extending Teacher
+interface Directors extends Teacher {
+  numberOfReports: number;
 }
 
 // Example usage as per requirements
@@ -23,5 +27,5 @@ const teacher3: Teacher = {
   contract: false, // This is allowed by the index signature [propName: string]: any
 };
 
-console.log(teacher3);
+console.log(director1);
 
