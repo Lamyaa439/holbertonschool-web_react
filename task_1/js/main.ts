9531a2c28a1ba14372e0c05226ab461897f318e7
Lamyaa <109660921+Lamyaa439@users.noreply.github.com>
2026-05-07 00:12:14 +0300

    Update main.ts

--- task_1/js/main.ts
+++ task_1/js/main.ts
@@ -22,4 +22,14 @@ const director1: Directors = {
   numberOfReports: 17,
 };
 
-console.log(director1);
+// Interface for the function
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+// The function implementation
+export const printTeacher: printTeacherFunction = (firstName: string, lastName: string): string => {
+  return `${firstName.charAt(0)}. ${lastName}`;
+};
+
+console.log(printTeacher("John", "Doe"));
