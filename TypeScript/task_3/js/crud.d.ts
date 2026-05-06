160eb1d841e12869db7b1df0e18ee1707475a428
Lamyaa <109660921+Lamyaa439@users.noreply.github.com>
2026-05-07 01:25:56 +0300

    Update crud.d.ts

--- TypeScript/task_3/js/crud.d.ts
+++ TypeScript/task_3/js/crud.d.ts
@@ -1,1 +1,8 @@
+import { RowID, RowElement } from './interface';
 
+/** 
+ * We declare the types for the functions exported in crud.js 
+ */
+export function insertRow(row: RowElement): RowID;
+export function deleteRow(rowId: RowID): void;
+export function updateRow(rowId: RowID, row: RowElement): RowID;
