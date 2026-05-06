ccfd694c97de0dcde4f2c12ec2613412a0c09411
Lamyaa <109660921+Lamyaa439@users.noreply.github.com>
2026-05-07 00:19:33 +0300

    task4: Update main.ts

--- task_1/js/main.ts
+++ task_1/js/main.ts
@@ -32,4 +32,47 @@ export const printTeacher: printTeacherFunction = (firstName: string, lastName: 
   return `${firstName.charAt(0)}. ${lastName}`;
 };
 
-console.log(printTeacher("John", "Doe"));
+/**
+ * Interface describing the Class (The Instance side)
+ * This defines the methods that will be available on the created object.
+ */
+interface StudentClassInterface {
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+/**
+ * Interface describing the Constructor (The Static side)
+ * In TypeScript, we use 'new' to describe a contract for a class constructor.
+ */
+interface StudentConstructor {
+  new (firstName: string, lastName: string): StudentClassInterface;
+}
+
+/**
+ * The Class Implementation
+ * use 'implements' to ensure the class follows the StudentClassInterface.
+ */
+class StudentClass implements StudentClassInterface {
+  firstName: string;
+  lastName: string;
+
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  // Method returns a fixed string as per requirements
+  workOnHomework(): string {
+    return "Currently working";
+  }
+
+  // Method returns the student's first name
+  displayName(): string {
+    return this.firstName;
+  }
+}
+
+const student = new StudentClass("Lamyaa", "Alghaihab");
+console.log(student.displayName());      // Should print: Lamyaa
+console.log(student.workOnHomework());  // Should print: Currently working
